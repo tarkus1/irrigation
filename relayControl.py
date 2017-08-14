b123d7f9937d3adaf0c764eb0373b96b5ac0bd47
Mark Perrin <mark@makewavesinc.ca>
2017-08-14 16:53:01 -0600

    3 relays added

--- relayControl.py
+++ relayControl.py
@@ -1,8 +1,10 @@
-import gpiozero
+from gpiozero import OutputDevice
 import time
 import os
 import sys
 import RPi.GPIO as GPIO
-zone1 = OutputDevice(16)
-zone3 = OutputDevice(20)
-zone3 = OutputDevice(21)
+zone1 = OutputDevice(16,initial_value=False)
+zone2 = OutputDevice(20,initial_value=False)
+zone3 = OutputDevice(21,initial_value=False)
+
+print (zone1.value, zone2.value, zone3.value, end='\n')
