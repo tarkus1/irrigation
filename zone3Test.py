5e80c08b8b43e1814e90e173f936829085e61190
Mark Perrin <mark@makewavesinc.ca>
2019-06-01 23:13:05 -0600

    correct logic and time offset for zones dbConnect not working

--- zone3Test.py
+++ zone3Test.py
@@ -4,26 +4,25 @@ import subprocess
 import os, csv, re
 import sys
 import RPi.GPIO as GPIO
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 zone1 = OutputDevice(16, active_high=False, initial_value=False)
 zone2 = OutputDevice(20,active_high=False, initial_value=False)
 zone3 = OutputDevice(21,active_high=False, initial_value=False)
 
+off = datetime.now()+timedelta(seconds=10)
+print('Time off ', off.strftime('%H:%M:%S'))
+
+print(zone1.value, zone2.value, zone3.value, end='\n')
 
 
-print (zone1.value, zone2.value, zone3.value, end='\n')
-
-zone3.on()
-
-print("zone 3 on ", zone3.value)
-
-while True:
+while (datetime.now() < off):
     try:
         # zone1.toggle()
         # zone2.toggle()
-        zone3.on()
+        #zone3.on()
+        print("zone 3 on ", zone3.value)
 
         print (zone1.value, zone2.value, zone3.value, end='\n')
 
@@ -31,9 +30,11 @@ while True:
         print (e)
 
     finally:
-        sleep(900)
+        sleep(2)
+        print('Running', datetime.now().strftime('%H:%M:%S'))
 
-zone3.off()
+        
 
+#zone3.off()
 print("zone 3 off ", zone3.value)
-
+print('Actual time off ', datetime.now().strftime('%H:%M:%S'))
