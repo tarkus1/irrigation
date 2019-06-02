1e02f35f9b8de07d20763b3a9734be9392d971ee
Mark Perrin <mark@makewavesinc.ca>
2019-06-01 21:59:57 -0600

    with timing

--- zone3Test.py
+++ zone3Test.py
@@ -15,3 +15,25 @@ zone3 = OutputDevice(21,active_high=False, initial_value=False)
 
 print (zone1.value, zone2.value, zone3.value, end='\n')
 
+zone3.on()
+
+print("zone 3 on ", zone3.value)
+
+while True:
+    try:
+        # zone1.toggle()
+        # zone2.toggle()
+        zone3.on()
+
+        print (zone1.value, zone2.value, zone3.value, end='\n')
+
+    except Exception as e:
+        print (e)
+
+    finally:
+        sleep(900)
+
+zone3.off()
+
+print("zone 3 off ", zone3.value)
+
